3bae31fc33d63b201505487bda483913cde3e98c
Quboid <github@quboid.net>
2023-08-17 19:19:52 +0100

    Initial config

--- Code/QCommon/QCommon/Shared/QCommon.cs
+++ Code/QCommon/QCommon/Shared/QCommon.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace QCommonShared
+namespace QCommonLib
 {
     internal class QCommon
     {
---
+++ Code/QCommon/QCommon/Shared/QLogger.cs
@@ -0,0 +1,27 @@
+﻿using Colossal.Logging;
+using System;
+using System.Diagnostics;
+
+namespace QCommonLib
+{
+    internal class QLogger
+    {
+        public static ILog ILogger = LogManager.GetLogger("CS2Test"); // Log file location %AppData%\..\LocalLow\Colossal Order\Cities Skylines II\
+
+        [Conditional("DEBUG")]
+        public static void LogDebugInfo(string message)
+        {
+            ILogger.Log(Level.Debug, message, new DebugException("CS2Test DebugException 03"));
+        }
+
+        public static void LogInfo(string message)
+        {
+            ILogger.Log(Level.Debug, message, new DebugException("CS2Test DebugException 04"));
+        }
+    }
+
+    public class DebugException : Exception
+    {
+        public DebugException(string message) : base(message) { }
+    }
+}
