84db7fd8248e89e38ec2919a40991d80d1fa5ee3
Quboid <github@quboid.net>
2023-08-22 18:30:45 +0100

    QKeyListener improvement

--- Code/QCommon/QCommon/Shared/QKeyListener.cs
+++ Code/QCommon/QCommon/Shared/QKeyListener.cs
@@ -1,23 +1,49 @@
 ﻿using Colossal.Serialization.Entities;
 using Game;
 using Game.Common;
-using System;
-using Unity.Burst.Intrinsics;
-using Unity.Collections;
-using Unity.Entities;
-using UnityEngine;
 using Game.Input;
 using Game.Prefabs;
 using Game.Tools;
+using System;
+using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using Unity.Collections;
+using Unity.Entities;
 using Unity.Jobs;
+using UnityEngine;
 
 namespace QCommonLib
 {
+    internal struct QKeyEvent
+    {
+        internal KeyCode m_code;
+        internal EventModifiers m_modifiers;
+        internal bool clicked;
+        internal float timeLastClicked;
+
+        internal QKeyEvent(KeyCode code, EventModifiers modifiers)
+        {
+            m_code = code;
+            m_modifiers = modifiers;
+            clicked = false;
+            timeLastClicked = 0;
+        }
+    }
+
+    internal enum QKeyListerContexts
+    {
+        DefaultTool,
+        InTool
+    }
+
     internal class QKeyListener : MonoBehaviour
     {
         private readonly float clickDelay = 0.3f;
         private bool clicked;
         private float timeLastClicked;
+        private List<QKeyEvent> keys;
+
+
         public static readonly KeyCode keyCode = KeyCode.M;
         public static readonly EventModifiers modifiers = EventModifiers.Control;
 
@@ -27,7 +53,20 @@ namespace QCommonLib
 
         public void OnGUI()
         {
+            if (UnityEngine.Event.current.type != EventType.KeyDown)
+            {
+                return;
+            }
+
             UnityEngine.Event current = UnityEngine.Event.current;
+            if (current == null)
+            {
+                QLoggerStatic.Debug($"Event: <null>");
+            }
+            else
+            {
+                QLoggerStatic.Debug($"Event: {current.type} {current.keyCode}");
+            }
             if (current.type == EventType.KeyDown && current.control && current.keyCode == keyCode && Time.time - timeLastClicked > clickDelay)
             {
                 clicked = true;
