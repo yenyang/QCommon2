92fb46c9aa51da0e9894a20c2481c3d45a029c2e
Quboid <github@quboid.net>
2023-09-08 12:43:42 +0100

    Keyboard handler

---
+++ Code/QCommon/QCommon/Shared/QKeyboard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.InputSystem;
+
+namespace QCommonLib
+{
+    internal static class QKeyboard
+    {
+        internal static Keyboard m_Keyboard;
+        private static UnityEngine.InputSystem.Controls.ButtonControl _alt, _shift, _control;
+
+        internal static void Init()
+        {
+            m_Keyboard = InputSystem.GetDevice<Keyboard>();
+            _alt = m_Keyboard.altKey;
+            _control = m_Keyboard.ctrlKey;
+            _shift = m_Keyboard.shiftKey;
+        }
+
+        internal static bool Alt => _alt.isPressed;
+
+        internal static bool Control => _control.isPressed;
+
+        internal static bool Shift => _shift.isPressed;
+    }
+}
