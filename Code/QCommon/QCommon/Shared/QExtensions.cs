d6500b36290def007ccbadd3e7848899dec3363c
Quboid <github@quboid.net>
2023-12-24 07:48:00 +0000

    Refactor

--- Code/QCommon/QCommon/Shared/QExtensions.cs
+++ Code/QCommon/QCommon/Shared/QExtensions.cs
@@ -22,7 +22,7 @@ namespace QCommonLib
         public static string RemoveWhitespace(this string input)
         {
             return new string(input.ToCharArray()
-                .Where(c => !Char.IsWhiteSpace(c))
+                .Where(c => !char.IsWhiteSpace(c))
                 .ToArray());
         }
 
