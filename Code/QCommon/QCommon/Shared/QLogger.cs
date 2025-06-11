c11f6be1b2f7a25d1ef18978e8ae43c1bd7c34cc
Quboid <github@quboid.net>
2025-06-11 08:12:55 +0100

    Switched from C# 10.0 to C# 9.0

--- Code/QCommon/QCommon/Shared/QLogger.cs
+++ Code/QCommon/QCommon/Shared/QLogger.cs
@@ -452,7 +452,7 @@ namespace QCommonLib
             string timezone = "-Unknown";
             try
             {
-                TimeSpan ts = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now);
+                TimeSpan ts = TimeZoneInfo.Local.GetUtcOffset(DateTime.Now);// TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now);
                 timezone = $"{ts.Hours}:{ts.Minutes:D2}";
                 if (ts.Hours > 0 || (ts.Hours == 0 && ts.Minutes > 0))
                 {
